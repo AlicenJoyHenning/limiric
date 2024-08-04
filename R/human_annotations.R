ee9b4809c903a05e5aa42987d5b710933498e1d8
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-08-04 23:03:07 +0200

    Add files via upload

---
+++ R/human_annotations.R
@@ -0,0 +1,9 @@
+#' Annotations
+#'
+#' Gene annotations for human (Homo sapiens).
+#'
+#' @source AnnotationHub
+#'
+#' @examples
+#' data(human_annotations)
+"human_annotations"
---
+++ R/mouse_annotations.R
@@ -0,0 +1,9 @@
+#' Annotations
+#'
+#' Gene annotations for mouse (Mus musculus).
+#'
+#' @source AnnotationHub
+#'
+#' @examples
+#' data(mouse_annotations)
+"mouse_annotations"
